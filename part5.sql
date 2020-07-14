6108e73f1d792b2e90dc38fdcc5652e2d1227fe5
Laura Godinez <issagodinez@Lauras-MacBook-Pro.local>
2020-07-14 00:58:44 -0400

    part5

--- part5.sql
+++ part5.sql
@@ -1,6 +1,8 @@
-SELECT CONCAT(e.firstName, " " , e.lastName) AS "Sales Rep" FROM employees e
-LEFT JOIN customers c
-ON e.employeeNumber = c.salesRepEmployeeNumber;
 
-
--- I dont really know how i can get the rest done.
+COUNT(o.orderNumber) AS "# Orders", IFNULL((od.quantityOrdered * od.priceEach), 0) AS "Total Sales"
+FROM employees e JOIN customers c ON e.employeeNumber=c.salesRepEmployeeNumber
+JOIN orders o ON o.customerNumber=c.customerNumber
+JOIN orderdetails od ON od.orderNumber=o.orderNumber
+WHERE e.jobTitle="Sales Rep"
+GROUP BY 1
+ORDER BY 3 DESC;
